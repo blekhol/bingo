3f631d90287f1dd7e44dc7cdb033b5073c5449b3
blekhol <peiszr@gmail.com>
2026-03-04 08:40:02 +0100

    BingoJatekos class

---
+++ bingo/BingoJatekos.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bingo
+{
+	internal class BingoJatekos
+	{
+		string nev;
+		string[] kartya;
+		List<int> talalatok; //indexek
+
+		public BingoJatekos(string nev, string[] kartya)
+		{
+			this.nev = nev;
+			this.kartya = kartya;
+			this.talalatok = [13];
+		}
+
+
+	}
+}
